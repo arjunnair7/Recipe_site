c7899e2cbe574be2d68f4386d47d9f556d75191e
Arjun R Nair <arjunrnair323@gmail.com>
2023-08-20 18:49:34 +0530

    Initial commit

---
+++ delicious/src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
+import Category from "./components/Category";
+import Pages from "./pages/Pages";
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Category/>
+      <Pages/>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
